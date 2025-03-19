a791e7c7fee71839de654dc47ee85424f72128bd
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-03-19 10:34:20 +0100

    initial commit

---
+++ src/model/User.java
@@ -0,0 +1,24 @@
+package model;
+
+public class User {
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+}
