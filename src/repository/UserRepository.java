4d0f63716d92ddbd2973e9cad541a1a3b8b03010
Panfilovs <maksims_panfilovs@yahoo.com>
2025-03-20 12:02:08 +0100

    userRepository

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package repository;
 
+import model.User;
+
 public interface UserRepository {
+
+    User addUser(String email, String password);
+
+    User addReadUser(User user, String book);
+
+    boolean isEmailExist(String email);
+
+    User getUserByEmail(String email);
+
+    boolean updatePassword(String email, String newPassword);
+
+
+
+
+
+
 }
