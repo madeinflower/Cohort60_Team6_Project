16119b8041e14dd458e7446c0f245e9d984bd35e
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-03-20 16:16:29 +0100

    Book,User,Role classes updated

--- src/model/User.java
+++ src/model/User.java
@@ -2,9 +2,13 @@ package model;
 
 public class User {
     private String email;
+    private String password;
+    private Role role;
 
-    public User(String email) {
+    public User(String email, String password) {
         this.email = email;
+        this.password = password;
+        this.role = Role.USER;
     }
 
     public String getEmail() {
@@ -15,12 +19,31 @@ public class User {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean takeBook() {
+        return role == Role.USER;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "email='" + email + '\'' +
+                ", role=" + role +
                 '}';
     }
-
- // test 1
 }
