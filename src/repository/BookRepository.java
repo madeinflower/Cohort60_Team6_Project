5fec78172b5e791fab33c1511ac257356d2926e6
T.Tatsenko <tatsenko.tetiana@gmail.com>
2025-03-20 11:41:30 +0100

    BookRepository, BookImpl classes added

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -1,4 +1,14 @@
 package repository;
 
 public interface BookRepository {
+
+    Book addBook(String title, String author);
+
+    MyList<Book> getAllBooks();
+    MyList<Book> searchByTitleOrAuthor(String query);
+    MyList<Book> getAvailableBooks();
+    MyList<Book> getTakenBooks();
+
+    void takeBook(Book book);
+    void returnBook(Book book);
 }
